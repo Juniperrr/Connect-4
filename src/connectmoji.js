32bc8955db794788fe84ba9d77895ef33a79b4a6
Juniperrr <sohnjunhee2011@gmail.com>
2019-09-16 03:54:08 -0400

    Add setCells, boardToString, and letterToCol functions

--- src/connectmoji.js
+++ src/connectmoji.js
@@ -25,3 +25,46 @@ module.exports.setCell = function (board, row, col, value) {
     new_board.data[module.exports.rowColToIndex(board, row, col)] = value;
     return new_board;
 }
+
+module.exports.setCells = function (board, ...moves) {
+    new_board = board;
+    for (move of moves) {
+        new_board = module.exports.setCell(new_board, move.row, move.col, move.val);
+    }
+    return new_board;
+}
+
+module.exports.boardToString = function (board) {
+    for (var i = 0; i < board.data.length; i++) {
+        var entry = board.data[i];
+        var character = entry === null ? " " : entry;
+        string += "| " + character + " ";
+        if (i % board.cols)
+            string += "|\n";
+    }
+    for (var i = 0; i < board.cols; i ++) {
+        prefix = "+";
+        suffix = "+";
+        if (i == 0) {
+            prefix = "|"
+        }
+        if (i == board.cols - 1) {
+            suffix = "|"
+        }
+        string += prefix + "-" + suffix;
+    }
+    for (var i = 0; i < board.cols; i ++) {
+        string += "| " + String.fromCodePoint(65 + i) + " ";
+    }
+    string += "|";
+    return string;
+}
+
+module.exports.letterToCol = function (letter) {
+    if (letter.length != 1)
+        return null;
+    var col = letter.charCodeAt(0) - 65;
+    if (col < 0 || col > 25)
+        return null;
+    return col;
+}
